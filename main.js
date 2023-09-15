de99ba1df7a80423fb8f5bf038fc837663f71a1d
Brandon D McAbee <bdmm18@gmail.com>
2023-09-15 11:13:28 -0400

    added new stuff

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 'use strict';
 
+const categoryListFormEl = document.querySelector('#categoryListForm');
+
 document.addEventListener('DOMContentLoaded' , function(){
     console.log('loaded')
 
     const chuckQuote = document.querySelector('#chuckQuote');
 
-     fetch("https://api.chucknorris.io/jokes/random?category=dev")
-     .then(function(response){
-        return response.json();
+    const apiUrl = 'https://api.chucknorris.io/jokes/random?category=dev';
+    get(apiUrl).then(function (response){
+        console.log('RESPONSE' , response)
+        showQuote(response.value , chuckQuote);
     })
-    .then(function(data) {
-        showQuote(data.value, chuckQuote);
-        return data;
-    });
 
+    const categoriesUrl = 'https://api.chucknorris.io/jokes/categories';
+    get(categoriesUrl).then(function(response){
+        generateCategoryList(response)
+        console.log(response)
+    })
 });
 function showQuote(quote, element) {
     element.innerText = quote;
 };
+
+function generateCategoryList(categoryMilk) {
+    console.log(categoryMilk);
+    const selectEl = document.createElement('select');
+    categoryMilk.map(function(category){
+        // create an option element
+    const option = document.createElement('option');
+     // define option attributes
+    option.value = category;
+    option.text = category;
+    // append the option to the <select>
+    selectEl.appendChild(option);
+    });
+    // append the <select> to the <form>
+    categoryListFormEl.append(selectEl);
+}
